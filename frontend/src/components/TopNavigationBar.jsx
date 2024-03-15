82ecc23a15d233dc44407e91ad2af86f490c7fa0
Racingwind <louis.k.hu@gmail.com>
2024-03-15 01:55:42 -0700

    passed favCount state info as prop to the TopNavigationBar component which then passes it as isFavPhotoExist prop down to FavBadge component which is used to determine if an alert about favorited photo should be displayed.

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,14 +4,12 @@ import FavBadge from './FavBadge';
 
 import '../styles/TopNavigationBar.scss'
 
-const isFavPhotoExist = true;
-
 const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={props.topics}/>
-      <FavBadge isFavPhotoExist={isFavPhotoExist} />
+      <FavBadge isFavPhotoExist={props.favCount} />
     </div>
   )
 }
